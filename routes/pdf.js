14c86549206b7044614c84c7e2dae96a24a9a4f6
wjjboy <285701156@qq.com>
2015-09-18 12:41:36 +0800

    test 1

--- routes/pdf.js
+++ routes/pdf.js
@@ -6,7 +6,7 @@ var wkhtmltopdf = require('wkhtmltopdf');
 /* GET users listing. */
 router.post('/create', function(req, res, next) {
 	var url = req.body.url;
-	console.log(" ip: " + req.ip + " url: " + url);
+	console.log("ip: " + req.ip + " url: " + url);
 	if(url){
 		wkhtmltopdf.command = wkhtmltopdfPath;
 		try {
